411758b46a57c7ca6aadcd9c3142d22bca2e95b5
aimaimaim <jadnipats@hotmail.com>
2021-04-22 19:18:01 +0700

    Done

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import MyTable from './MyTable';
+
+
+const App = () =>{
+    return (
+        <div >
+           {/* <Button variant="contained">Default</Button> */}
+           <MyTable/>
+           
+        </div>
+    )
+}
+
+
+export default App;
