89a3e574a1fde437d1abde08daf2e178eb16f50b
aimaimaim <jadnipats@hotmail.com>
2021-04-25 08:19:30 +0700

    merge

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,26 +2,12 @@ import React from 'react';
 
 class Navbar extends React.Component {
 
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //       date: null
-    
-    //     };
-    //   }
-
-    
     render(){
 
-        // console.log(new Date())
-        // const {date} = this.state;
         return(
             <div>
                 <h1>Code Monday Assignment</h1>
-                
-             {/* <b>Today is: {date=>new Date()}</b> */}
             </div>
-
         );
     }
 }
